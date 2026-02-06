8c0c57262effd7c2edf95d844acfd2787b9e27c9
Douglas-code-design <douglas.n.oliveira@outlook.com>
2026-02-05 21:55:23 -0300

    Add files via upload

---
+++ cases.py
@@ -0,0 +1,33 @@
+# # isapproved = False
+# # istest = True
+# # name = "maria"
+# # age = 20 
+# # heigt = 1.97
+
+# # print(isapproved)
+# # print(istest)
+# # print(name)
+# # print(age)
+# # print(heigt)
+# # print(isapproved, istest, name, age, heigt)
+# # print(f'{isapproved},{istest}')
+
+
+# nome = "Luiza Martins"
+# idade = 37
+# profissão = "design research"
+
+# (fstring = f"oi eu sou a {nome}, tenho {idade} e minha profissão é {profissão}")
+
+# print(fstring)
+
+# numero_one = input("digite um numero :  ")
+# numero_two = input("digite outro numero:  ")           
+
+# print(type(numero_one))
+# print(type(numero_two))
+
+# print(int(numero_one) + int(numero_two))
+
+# str(numero_one)
+# float(numero_two)
